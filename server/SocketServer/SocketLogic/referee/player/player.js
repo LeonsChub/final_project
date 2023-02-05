8a8c430f63bed6dffb12322f632efcb59bd8170d
Leon <pavlenkoleon@gmail.com>
2023-02-05 09:56:12 +0200

    merge

--- server/SocketServer/SocketLogic/referee/player/player.js
+++ server/SocketServer/SocketLogic/referee/player/player.js
@@ -1,20 +1,41 @@
-const Player = (name, id, chips = 1000) => {
-  const player = {
-    name: name,
-    id: id,
-    chips: chips,
-    stake: 0,
-    stakeGap: 0,
-    cards: [],
-    score: [],
-    fold: false,
-    ready: false,
-    allIn: false,
-    specialAttrs: [],
-    win: false,
-  };
+class Player {
+  constructor(name, id, chips = 1000) {
+    this.name = name;
+    this.id = id;
+    this.chips = chips;
+    this.stake = 0;
+    this.stakeGap = 0;
+    this.cards = [];
+    this.score = [];
+    this.fold = false;
+    this.ready = false;
+    this.allIn = false;
+    this.specialAttrs = [];
+    this.win = false;
+    this.minEntry = 20;
+  }
 
-  return player;
-};
+  setCards(cards) {
+    if (this.cards.length < 2) {
+      this.cards = [...cards];
+    }
+  }
+  setSmallBlind() {
+    // set player as being the small blind
+    this.specialAttrs.push("small blind");
+    this.stake = this.minEntry / 2;
+    this.chips = this.chips - this.minEntry / 2;
+
+    return this.minEntry / 2;
+  }
+  setBigBlind() {
+    // set player as being the small blind
+    this.specialAttrs.push("big blind");
+    this.stake = this.minEntry;
+    this.chips = this.chips - this.minEntry;
+
+    return this.minEntry;
+  }
+}
 
 module.exports = Player;
--- server/SocketServer/SocketLogic/referee/pokerRef.js
+++ server/SocketServer/SocketLogic/referee/pokerRef.js
@@ -13,43 +13,49 @@ const {
 const Player = require("./player/player");
 
 const pokerRef = (socket, io, roomId) => {
-  const gameState = {
-    deck: shuffleDeck(initDeck()),
-    players: generatePlayerData(roomId),
-    community: [],
-    roundInfo: {},
-    gameStage: "",
-  };
-
-  io.to(roomId).emit("ref ready", gameState);
-
-  gameState.players.forEach((player) => {
-    const hand = [];
-    hand.push(gameState.deck.pop());
-    hand.push(gameState.deck.pop());
-
-    updatePlayerHand(player.id, hand);
-
-    // setTimeout(() => {
-    //   io.to(roomId).emit("handing cards", { to: player.id, hand });
-    // }, 750);
-  });
-  // console.log(`Emitting new state ${JSON.stringify(gameState)}`);
-  io.to(roomId).emit("handing cards", gameState);
-
-  // --------------------------------
-
   function updatePlayerHand(to, hand) {
     const index = gameState.players.findIndex((player) => {
       return player.id === to;
     });
 
-    if (gameState.players[index].cards.length < 2) {
-      gameState.players[index].cards.push(hand[0]);
-      gameState.players[index].cards.push(hand[1]);
-    }
+    gameState.players[index].setCards(hand);
   }
+
+  function addToPot(amount) {
+    gameState.pot += amount;
+  }
+  const gameState = {
+    deck: shuffleDeck(initDeck()),
+    players: generatePlayerData(roomId),
+    community: [],
+    burned: [],
+    roundInfo: {
+      sbIndex: 0, //small blind index
+      bbIndex: 1, //bbindex
+    },
+    pot: 0,
+    gameStage: "preflop",
+  };
+
+  io.to(roomId).emit("ref ready", gameState);
+
+  //determine blinds
+  const { sbIndex, bbIndex } = gameState.roundInfo;
+  addToPot(gameState.players[sbIndex].setSmallBlind()); // get small blind entry and add it to pot in game state
+  addToPot(gameState.players[bbIndex].setBigBlind()); // get small blind entry and add it to pot in game state
+  //hand cards to each player
+  gameState.players.forEach((player) => {
+    const hand = [];
+    hand.push(gameState.deck.pop()); // pop first to players and append them to an array
+    hand.push(gameState.deck.pop());
+
+    updatePlayerHand(player.id, hand); // update players array append hand card to player with id given
+  });
+
+  io.to(roomId).emit("handing cards", gameState); // update
 };
+
+// --------------------------------Helper functions
 
 function initDeck() {
   const suits = ["clubs", "diamonds", "hearts", "spades"];
@@ -66,6 +72,7 @@ function initDeck() {
 }
 
 function shuffleDeck(deck) {
+  // fisher yates shuffling algorithm
   for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[j]] = [deck[j], deck[i]];
@@ -76,7 +83,7 @@ function shuffleDeck(deck) {
 function generatePlayerData(roomId) {
   roomData = getRoomById(roomId);
   const players = roomData.players.map((player) => {
-    return Player(player.name, player.id);
+    return new Player(player.name, player.id);
   });
   return players;
 }
